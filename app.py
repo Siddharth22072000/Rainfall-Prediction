fbe0bfff151e30c20aa3872423178349a8e280d0
Siddharth22072000 <siddharthpatel22072000@gmail.com>
2024-03-11 19:17:58 +0530

    save

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+import  pickle
+import numpy as np
+
+model = pickle.load(open('best_model.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    state = request.form.get('state')
+    year = int(request.form.get('year'))
+    month = int(request.form.get('month'))
+
+    input_query= np.array([[year, month]])
+    result= model.predict(input_query)[0]
+
+    return jsonify(str(result))
+if __name__ == '__main__':
+    app.run(debug=True)
